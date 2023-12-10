d710a1976a6cb0b9745ffe38dfcc8fc4a68a68dd
sneha kumari <snehakumari64445@gmail.com>
2023-12-10 16:31:33 +0530

    yoyo

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const COLORS = {
+    white: "#FFFFFF",
+    black: "#222222",
+    primary: "#473EB8",
+    secondary: "#473EB8",
+    grey: "#CCCCCC"
+}
+
+export default COLORS;
---
+++ screens/index.js
@@ -0,0 +1,8 @@
+import Login from "./Login";
+import Welcome from "./Welcome";
+import Signup from "./Signup";
+export {
+    Login,
+    Welcome,
+    Signup
+}
